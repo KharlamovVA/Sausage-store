f6f90c8c34c3d5e70eb722f8c1e4699c52beb01c
std-017-017 std-017-017 <std-017-017@praktikum-services.ru>
2023-05-19 12:14:51 +0300

    Deploy v. 3.5

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,9 +5,9 @@ set -xe
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
 sudo rm -f /home/front-user/frontend||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPOFRONT_URL}/${VERSION}/sausage-store-${VERSION}.tar.gz
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPOFRONT_URL}/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo cp ./sausage-store.tar.gz /home/front-user/frontend||true #"<...>||true" говорит, если команда обвалится — продолжай
-tar xvzf /home/front-user/frontend/sausage-store-${VERSION}.tar.gz
+tar xvzf /home/front-user/frontend/sausage-store.tar.gz
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
