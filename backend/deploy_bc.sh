5152349165546e7338af1d4db0c0f0e3b6b3cfb3
std-017-017 std-017-017 <std-017-017@praktikum-services.ru>
2023-05-21 13:09:00 +0300

    Deploy v. 5.0.0

--- backend/deploy_bc.sh
+++ backend/deploy_bc.sh
@@ -2,11 +2,11 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-sudo rm -f /home/jarservice/sausage-store.jar||true
+sudo cp -rf /home/${DEV_USER}/sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo rm -f /home/jarservice/sausage-store-${VERSION}.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPOBACK_URL}/${VERSION}/sausage-store-${VERSION}.jar
-sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
+sudo cp /home/jarservice/sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
--- frontend/deploy_fr.sh
+++ frontend/deploy_fr.sh
@@ -3,11 +3,11 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
-sudo rm -f /home/front-user/sausage-store.tar.gz||true
+sudo cp -rf /home/${DEV_USER}/sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
+sudo rm -f /home/front-user/sausage-store-${VERSION}.tar.gz||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPOFRONT_URL}/${VERSION}/sausage-store-${VERSION}.tar.gz
-sudo cp ./sausage-store.tar.gz /home/front-user/sausage-store.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай
+sudo cp /home/front-user/sausage-store-${VERSION}.tar.gz /home/front-user/sausage-store.tar.gz||true #"<...>||true" говорит, если команда обвалится — продолжай
 sudo tar xzf /home/front-user/sausage-store.tar.gz
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
