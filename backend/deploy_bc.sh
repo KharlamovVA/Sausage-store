e963833a1b100033617531c186e9fd981bce72a4
std-017-017 std-017-017 <std-017-017@praktikum-services.ru>
2023-05-21 13:33:37 +0300

    Deploy v. 5.0.7

--- backend/deploy_bc.sh
+++ backend/deploy_bc.sh
@@ -3,7 +3,7 @@
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf /home/${DEV_USER}/sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-sudo rm -f /home/jarservice/sausage-store-${VERSION}.jar||true
+#sudo rm -f /home/jarservice/sausage-store-${VERSION}.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPOBACK_URL}/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp /home/jarservice/sausage-store-${VERSION}.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
